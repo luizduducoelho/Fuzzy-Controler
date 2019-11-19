ad369228b484acf41cd6a49cb8dd410b75335bee
Luiz Eduardo Lima Coelho <luizelcoelho@gmail.com>
2019-11-19 20:40:36 -0300

    Check fuzzy rules

--- planta.m
+++ planta.m
@@ -9,19 +9,15 @@ h22 = tf([-1.096], [75 1]);
 H = [h11, h12; h21, h22];
 
 L1 = 0.5;
-c11 = -0.5;
 c12 = 0;
-c13 = 0.5;
+c11 = c12 - L1/2;
+c13 = c12 + L1/2;
 
 flc1 = readfis('flc1.fis');
 flc1.input.mf(1).params = [c11-L1/2, c11, c11+L1/2];
 flc1.input.mf(2).params = [c12-L1/2, c12, c12+L1/2];
 flc1.input.mf(3).params = [c13-L1/2, c13, c13+L1/2];
 
+flc2 = readfis('flc2.fis');
 
-
-
-
-
-
-
+sim('control_diagram_simple.slx')
