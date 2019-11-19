66dcf0556632adf5d927572887a42498a0d190bf
Luiz Eduardo Lima Coelho <luizelcoelho@gmail.com>
2019-11-19 18:52:16 -0300

    first commit

---
+++ planta.m
@@ -0,0 +1,27 @@
+clear;
+clc;
+close all;
+
+h11 = tf([0.878], [75 1]);
+h12 = tf([-0.864], [75 1]);
+h21 = tf([1.082], [75 1]);
+h22 = tf([-1.096], [75 1]);
+H = [h11, h12; h21, h22];
+
+L1 = 0.5;
+c11 = -0.5;
+c12 = 0;
+c13 = 0.5;
+
+flc1 = readfis('flc1.fis');
+flc1.input.mf(1).params = [c11-L1/2, c11, c11+L1/2];
+flc1.input.mf(2).params = [c12-L1/2, c12, c12+L1/2];
+flc1.input.mf(3).params = [c13-L1/2, c13, c13+L1/2];
+
+
+
+
+
+
+
+
