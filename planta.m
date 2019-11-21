b5e4ce59db7d71ec0569ad2426d9b366ce0111d0
Luiz Eduardo Lima Coelho <luizelcoelho@gmail.com>
2019-11-21 20:38:28 -0300

    Diferential evolution

--- planta.m
+++ planta.m
@@ -8,16 +8,53 @@ h21 = tf([1.082], [75 1]);
 h22 = tf([-1.096], [75 1]);
 H = [h11, h12; h21, h22];
 
-L1 = 0.5;
+flc1 = readfis('flc1.fis');
+
+%{
+L1 = 1.2;
 c12 = 0;
 c11 = c12 - L1/2;
 c13 = c12 + L1/2;
+flc1.input(1).mf(1).params = [c11-L1/2, c11, c11+L1/2];
+flc1.input(1).mf(2).params = [c12-L1/2, c12, c12+L1/2];
+flc1.input(1).mf(3).params = [c13-L1/2, c13, c13+L1/2];
 
-flc1 = readfis('flc1.fis');
-flc1.input.mf(1).params = [c11-L1/2, c11, c11+L1/2];
-flc1.input.mf(2).params = [c12-L1/2, c12, c12+L1/2];
-flc1.input.mf(3).params = [c13-L1/2, c13, c13+L1/2];
+L2 = 1.2;
+c22 = 0;
+c21 = c22 - L2/2;
+c23 = c22 + L2/2;
+flc1.input(2).mf(1).params = [c21-L2/2, c21, c21+L2/2];
+flc1.input(2).mf(2).params = [c22-L2/2, c22, c22+L2/2];
+flc1.input(2).mf(3).params = [c23-L2/2, c23, c23+L2/2];
 
-flc2 = readfis('flc2.fis');
+LO = 10;
+co1 = -LO;
+co2 = -LO/2;
+co3 = 0;
+co4 = LO/2;
+co5 = LO;
+flc1.output.mf(1).params = [co1-LO/2, co1, co1+LO/2];
+flc1.output.mf(2).params = [co2-LO/2, co2, co2+LO/2];
+flc1.output.mf(3).params = [co3-LO/2, co3, co3+LO/2];
+flc1.output.mf(4).params = [co4-LO/2, co4, co4+LO/2];
+flc1.output.mf(5).params = [co5-LO/2, co5, co5+LO/2];
+%}
 
-sim('control_diagram_simple.slx')
+sim('siso1.slx')
+
+%{
+% plota
+figure()
+subplot(4,1,1);
+plot(yout{1}.Values.time, yout{1}.Values.data)
+title("Saída")
+subplot(4,1,2);
+plot(yout{2}.Values.time, yout{2}.Values.data)
+title("Erro")
+subplot(4,1,3);
+plot(yout{3}.Values.time, yout{3}.Values.data)
+title("Sinal de controle")
+subplot(4,1,4);
+plot(yout{4}.Values.time, yout{4}.Values.data)
+title("Derivada do erro")
+%}
---
+++ testa_custo.m
@@ -0,0 +1,8 @@
+clear;
+clc;
+close all;
+
+global flc1;
+flc1 = readfis('flc1.fis');
+
+custo([10; 1.05; 1.1])
